0c7e7b9096d884ca2ef57606a20ecd4445544704
Jerry Blaauw <jerryblaauw@hotmail.com>
2020-07-30 16:59:34 +0200

    ik ben een aap

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ module.exports = (router, client) => {
   router.post('/jira/' + process.env.JIRA_SECRET, (req, res) =>
     JiraWebhookHandler.handle(req, res, client)
   ),
-  router.get('/test', () => {
+  router.get('/test', (req, res) => {
     res.send('hij doet het');
   })
 
