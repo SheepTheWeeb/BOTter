9b60fa3b6362046b824c43c90fbe210c56fb84c3
jerraloid <jerryblaauw@hotmail.com>
2020-07-30 22:49:22 +0200

    dikke handler

--- src/handlers/JiraWebhookHandler.js
+++ src/handlers/JiraWebhookHandler.js
@@ -3,26 +3,32 @@ module.exports = {
 
     console.log(req.body)
 
-    //grab otter discord server and log channel
-    var otterGuild = client.guilds.cache.find(guild => guild.id === '730511256682889319');
-    var logChannel = otterGuild.channels.cache.find(channel => channel.id === '738358619032256593');
+    try {
+      //grab otter discord server and log channel
+      var otterGuild = client.guilds.cache.find(guild => guild.id === '730511256682889319');
+      var logChannel = otterGuild.channels.cache.find(channel => channel.id === '738358619032256593');
 
-    //create message
-    var description = `Key: ${req.body.issue.key}` +
-      `\nTitle: ${req.body.issue.fields.summary}` +
-      `\nBy: ${req.body.user.name}` +
-      `\nRead more: ${req.body.issue.self}`;
+      //create message
+      var description = `Key: ${req.body.issue.key}` +
+        `\nTitle: ${req.body.issue.fields.summary}` +
+        `\nBy: ${req.body.user.displayName}` +
+        `\nRead more: ${req.body.issue.self}`;
 
-    //create embed
-    const embed = new Discord.MessageEmbed()
+      //create embed
+      const embed = new Discord.MessageEmbed()
         .setColor('#0088ff')
-        .setTitle(`${req.body.webhookEvent} ${emojiLookup.get("HANDSUP")}`)
+        .setTitle(`${req.body.issue_event_type_name} ${emojiLookup.get("HANDSUP")}`)
         .setTimestamp()
         .setFooter('Ik ben er helemaal klaar mee... pls send help')
         .setDescription(description);
 
-    logChannel.send(embed);
+      logChannel.send(embed);
 
-    res.sendStatus(200);
+      res.sendStatus(200);
+
+    } catch (ex) {
+      console.log(ex.message);
+      res.sendStatus(500);
+    }
   }
 }
