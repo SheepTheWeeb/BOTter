2170397b189df93812678ef344e360489f0cb267
Jerry Blaauw <jerryblaauw@hotmail.com>
2020-07-31 11:08:41 +0200

    added changelog to jira webhook message

--- src/handlers/JiraWebhookHandler.js
+++ src/handlers/JiraWebhookHandler.js
@@ -8,21 +8,49 @@ module.exports = {
       var otterGuild = client.guilds.cache.find(guild => guild.id === '730511256682889319');
       var logChannel = otterGuild.channels.cache.find(channel => channel.id === '738358619032256593');
 
-      //create message
+      //only grab first name and picture
       var jiraName = req.body.user.displayName.split(" ")[0];
       var jiraPics = req.body.user.avatarUrls;
+
+      //create readable message fore webhookevent type
+      var eventType = '';
+      switch(req.body.webhookEvent) {
+        case 'jira:issue_created':
+          eventType = 'Ticket Created';
+          break;
+        case 'jira:issue_updated':
+          eventType = 'Ticket Updated';
+          break;
+        case 'jira:issue_deleted':
+          eventType = 'Ticket Deleted';
+          break;
+        default:
+          eventType = req.body.webhookEvent;
+      }
 
       //create embed
       const embed = new Discord.MessageEmbed()
         .setColor('#0088ff')
         .setAuthor(jiraName, jiraPics[Object.keys(jiraPics)[0]])
-        .setTitle(`${req.body.issue.key} | ${req.body.webhookEvent} ${emojiLookup.get("HANDSUP")}`)
+        .setTitle(`${eventType}: ${req.body.issue.key} ${emojiLookup.get("HANDSUP")}`)
         .setTimestamp()
         .setFooter('Ik ben er helemaal klaar mee...')
         .setDescription(req.body.issue.fields.summary);
 
-      logChannel.send(embed);
+      //check if there is a changelog
+      if(req.body.changelog) {
 
+        //put every change into a field
+        req.body.changelog.items.forEach(item => {
+          //to prevent doxxing :monkaS:
+          if(item.field === 'reporter') return;
+
+          embed.addField(item.field, `**Changed from:** ${item.fromString}\n**Changed to:** ${item.toString}`, true);
+        });
+      }
+
+      //send message and completed response status
+      logChannel.send(embed);
       res.sendStatus(200);
 
     } catch (ex) {
