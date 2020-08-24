6ff5115c3757392490c74514f783e1581b12f65e
jerraloid <jerryblaauw@hotmail.com>
2020-08-24 20:13:55 +0200

    added init migration

--- migrations/20200824145321-databaseinit.js
+++ migrations/20200824145321-databaseinit.js
@@ -2,20 +2,87 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('otteruser', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      discord_id: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      discord_tag: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: Sequelize.NOW
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: Sequelize.literal(
+          'CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3)'
+        )
+      }
+    });
+
+    await queryInterface.createTable('redflag', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'otteruser',
+          key: 'id'
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade'
+      },
+      received_from: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'otteruser',
+          key: 'id'
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade'
+      },
+      reason: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      double_red: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: Sequelize.NOW
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: Sequelize.literal(
+          'CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3)'
+        )
+      }
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('redflag');
+    await queryInterface.dropTable('otteruser');
   }
 };
--- src/config/config.ts
+++ src/config/config.ts
@@ -6,20 +6,23 @@ export default {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    port: 3306
+    port: 3306,
+    dialect: 'mysql'
   },
   test: {
     username: 'root',
     password: null,
     database: 'database_test',
     host: '127.0.0.1',
-    port: 3306
+    port: 3306,
+    dialect: 'mysql'
   },
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    port: 50368
+    port: 50368,
+    dialect: 'mysql'
   }
 };
