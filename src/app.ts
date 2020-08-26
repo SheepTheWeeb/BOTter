94cd5832b14413680425bdca4450386f9d6d233a
Jerry Blaauw <jerryblaauw@hotmail.com>
2020-08-26 10:19:45 +0200

    updated types app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 require('dotenv').config();
 import appInsights = require('applicationinsights');
 import Discord from 'discord.js';
-import express from 'express';
+import express, { NextFunction, Request, Response, Router } from 'express';
 import bodyParser from 'body-parser';
 import routes from './routes';
 import MessageHandler from './handlers/MessageHandler';
@@ -9,30 +9,30 @@ import EmojiLookup from './commands/EmojiLookup';
 import CommandLookup from './commands/CommandLookup';
 import { initializeDb } from './models/Init';
 
-const client = new Discord.Client();
+const client: Discord.Client = new Discord.Client();
 const app = express();
 app.use(bodyParser.json());
 initializeDb();
 // load in MessageHandler
-const messageHandler = new MessageHandler(process.env.PREFIX);
+const messageHandler: MessageHandler = new MessageHandler(process.env.PREFIX);
 
 //Init Microsoft Application Insights
 appInsights.setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY).start();
 
-const insightsClient = new appInsights.TelemetryClient(
+const insightsClient: appInsights.TelemetryClient = new appInsights.TelemetryClient(
   process.env.APPINSIGHTS_INSTRUMENTATIONKEY
 );
 export { insightsClient };
 // load in emojiLookup and commandlookup
-const emojiLookup = new EmojiLookup(client);
+const emojiLookup: EmojiLookup = new EmojiLookup(client);
 export { emojiLookup };
-const commandLookup = new CommandLookup();
+const commandLookup: CommandLookup = new CommandLookup();
 export { commandLookup };
 
 // init discord client
 client.on('ready', async () => {
   // setup webhook handler
-  let router: any = express.Router();
+  let router: Router = express.Router();
   app.use('/api', routes(router, client));
   app.listen(process.env.PORT);
 
@@ -40,7 +40,7 @@ client.on('ready', async () => {
 });
 
 // use message handler to handle messages
-client.on('message', (msg: any) => {
+client.on('message', (msg: Discord.Message) => {
   messageHandler.handle(msg);
 });
 
@@ -48,7 +48,7 @@ client.on('message', (msg: any) => {
 client.login(process.env.DISCORD_TOKEN);
 
 /* eslint-disable-next-line no-unused-vars */
-app.use((err: Error, req: any, res: any, next: any) => {
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.log(err);
   res.sendStatus(500);
 });
